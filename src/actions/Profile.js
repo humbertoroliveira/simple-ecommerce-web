a63492b7cb1d44379ec714ddb631d23473cf49d9
Humberto de Oliveira <55997445+humbertoroliveira@users.noreply.github.com>
2020-07-13 11:40:12 +0100

    adding profile page with saving capabilities and userId

--- src/actions/Profile.js
+++ src/actions/Profile.js
@@ -4,7 +4,3 @@ export const setProfile = (profile) => async (dispatch, getState) => {
     payload: profile,
   });
 };
-
-export const getPId = () => (dispatch, getState) => {
-  return getState().Profile;
-};
